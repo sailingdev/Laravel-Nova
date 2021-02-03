2a9eab78337838ee647987f251fe0f9495cee43c
Mona Moxie <moxie4lyf@gmail.com>
2021-02-03 15:51:31 +0100

    fix: breaking table header

--- nova-components/TypeDailyPerfWebsiteBreakDownTool/resources/js/tool.js
+++
@@ -1,9 +0,0 @@
-Nova.booting((Vue, router, store) => {
-  router.addRoutes([
-    {
-      name: 'type-daily-perf-website-break-down-tool',
-      path: '/type-daily-perf-website-break-down-tool',
-      component: require('./components/Tool'),
-    },
-  ])
-})
