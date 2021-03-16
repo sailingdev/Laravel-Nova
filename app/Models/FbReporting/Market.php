6bafdf29fed66039137b0101628870fd4b351ee0
Mona Moxie <moxie4lyf@gmail.com>
2021-03-16 17:25:54 +0100

    feature: market resource

---
+++ app/Models/FbReporting/Market.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\FbReporting;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Market extends Model
+{
+    use HasFactory;
+}
