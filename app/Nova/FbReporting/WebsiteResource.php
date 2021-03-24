26be94307f3e145640d6fbfd41755bd7bd6a1a0b
Mona Moxie <moxie4lyf@gmail.com>
2021-03-24 10:25:53 +0100

    feature: websites resource

---
+++ app/Nova/FbReporting/WebsiteResource.php
@@ -0,0 +1,135 @@
+<?php
+
+namespace App\Nova\FbReporting;
+
+use App\Nova\Resource;
+use Illuminate\Http\Request;
+use Laravel\Nova\Fields\ID;
+use Laravel\Nova\Fields\Text;
+use Laravel\Nova\Fields\Textarea;
+use Laravel\Nova\Http\Requests\NovaRequest;
+
+class WebsiteResource extends Resource
+{
+    /**
+     * The model the resource corresponds to.
+     *
+     * @var string
+     */
+    public static $model = \App\Models\FbReporting\Website::class;
+
+    /**
+     * Get the displayable label of the resource.
+     *
+     * @return string
+     */
+    public static function label()
+    {
+        return 'Websites';
+    }
+
+    /**
+     * Get the displayable singular label of the resource.
+     *
+     * @return string
+     */
+    public static function singularLabel()
+    {
+        return 'Website';
+    }
+
+
+    /**
+     * The single value that should be used to represent the resource when being displayed.
+     *
+     * @var string
+     */
+    public static $title = 'domain';
+
+    /**
+     * The columns that should be searched.
+     *
+     * @var array
+     */
+    public static $search = [
+        'domain', 'site_tag', 'feed'
+    ];
+
+    /**
+     * Get the fields displayed by the resource.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function fields(Request $request)
+    {
+        return [
+            ID::make(__('ID'), 'id')->sortable(),
+            Text::make('Domain')
+            ->sortable()
+            ->rules('required', 'max:255'),
+            Text::make('Site Tag')
+            ->sortable()
+            ->rules('required', 'max:255'),
+            Textarea::make('Supported Markets')
+            ->sortable()
+            ->rules('required'),
+            Text::make('Feed')
+            ->sortable()
+            ->rules('required', 'max:255'),
+            Text::make('Source Tag')
+            ->sortable()
+            ->rules('required', 'max:255'),
+            Text::make('Range ID ')
+            ->sortable()
+            ->rules('required', 'max:255'), 
+            Text::make('Ad Unit ID')
+            ->sortable()
+            ->rules('required', 'max:255'),
+        ];
+    }
+
+    /**
+     * Get the cards available for the request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function cards(Request $request)
+    {
+        return [];
+    }
+
+    /**
+     * Get the filters available for the resource.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function filters(Request $request)
+    {
+        return [];
+    }
+
+    /**
+     * Get the lenses available for the resource.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function lenses(Request $request)
+    {
+        return [];
+    }
+
+    /**
+     * Get the actions available for the resource.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function actions(Request $request)
+    {
+        return [];
+    }
+}
---
+++ database/migrations/2021_03_24_091456_create_websites_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateWebsitesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('websites', function (Blueprint $table) {
+            $table->id();
+            $table->string('domain');
+            $table->string('site_tag');
+            $table->text('supported_markets');
+            $table->string('feed');
+            $table->string('source_tag');
+            $table->string('range_id');
+            $table->string('ad_unit_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('websites');
+    }
+}
