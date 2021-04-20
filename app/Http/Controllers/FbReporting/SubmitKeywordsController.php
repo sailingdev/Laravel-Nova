f109819f568cb18b6ab4e5f30e1175167e9f5854
Mona Moxie <moxie4lyf@gmail.com>
2021-04-20 16:04:03 +0100

    unset test mode

--- app/Http/Controllers/FbReporting/SubmitKeywordsController.php
+++ app/Http/Controllers/FbReporting/SubmitKeywordsController.php
@@ -21,7 +21,7 @@ class SubmitKeywordsController extends Controller
     public function submit(SubmitKeywordsRequest $request, StringManipulator $sm, SubmittedKeywordService $sks)
     {
         $cotService = new \App\Services\CampaignOptimizeTrackerService;
-        dd($cotService->optimize());
+        // dd($cotService->optimize());
         $rawKeywords = $request->keywords; 
         $prepKeywords = $sm->generateArrayFromString(str_replace("\n", '<br />',  $rawKeywords), '<br />');
 
