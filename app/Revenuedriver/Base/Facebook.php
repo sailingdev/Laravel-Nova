86d6eb8efa587c3043b8bb24e781966c4263c5d0
Mona Moxie <moxie4lyf@gmail.com>
2021-06-15 02:27:29 +0100

    set campaign default status

--- app/Revenuedriver/Base/Facebook.php
+++ app/Revenuedriver/Base/Facebook.php
@@ -243,6 +243,7 @@ abstract class Facebook
      */
     public function determineStatus(string $status, string $targetAccount): string
     {
+        return 'PAUSED';
         if (config('app.env') === 'production' && $targetAccount != $this->accountRD28) {
             return 'ACTIVE';
         }
