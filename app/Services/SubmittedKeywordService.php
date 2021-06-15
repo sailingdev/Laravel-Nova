64c9e0e51d69b621a933a4a476a4318110277553
Mona Moxie <moxie4lyf@gmail.com>
2021-06-15 01:17:47 +0100

    clear unused

--- app/Services/SubmittedKeywordService.php
+++ app/Services/SubmittedKeywordService.php
@@ -824,9 +824,7 @@ class SubmittedKeywordService
         return SubmittedKeyword::where('batch_id', $batchId)->delete();
     }
     public function createCampaignFromTemplate(array $submittedKeywords, string $market)
-    {  
-        // $fb = new \App\Revenuedriver\FacebookPage;
-        // dd($fb->loadBusinessAccountPages());
+    {   
         $this->facebookCampaign->initRD();
         $campaignId = '23846614980830456';
         $facebookCampaign = new FacebookCampaign;
